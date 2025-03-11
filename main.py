81602c9b04c33592ac01568b73d9cffceda09540
N-jiranut <jiranut.nat@gmail.com>
2025-03-11 21:37:58 +0700

    first part
    lazy

--- main.py
+++ main.py
@@ -10,20 +10,32 @@ mpDraw = mp.solutions.drawing_utils
 
 pTime = 0
 cTime = 0
+wTime = time.time()
 
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
+    # print(results.multi_hand_landmarks)
     
     
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                cv2.circle(img, (cx, cy), 10, (0, 0, 0), cv2.FILLED)
 
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+    
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
+    
+    if int(cTime) - int(wTime) == 3:
+        print(results.multi_hand_landmarks)
+        wTime = cTime
+    # print(cTime-wTime)
 
     cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0,0,0), 3)
 
